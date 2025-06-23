09bae47d9f201981ddd83124b373a8af7dfeb994
Joel Kram <joelnskram@gmail.com>
2025-06-23 11:55:58 +0100

    added header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+export const Header = () => {
+  return (
+    <section className="header-container">
+      <h1>Nc News Site Here</h1>
+      <nav>
+        <ul>
+          <li>topic 1</li>
+          <li>topic 2</li>
+          <li>topic 3</li>
+        </ul>
+      </nav>
+    </section>
+  );
+};
