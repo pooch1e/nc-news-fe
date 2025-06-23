d06da6a58279a428cbbdbc9df62cc79760b511c7
Joel Kram <joelnskram@gmail.com>
2025-06-23 16:37:54 +0100

    added: footer and styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ArticlesPage } from './components/ArticlesPage';
 import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         <h2>Articles Area</h2>
         <ArticlesPage />
       </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 }
