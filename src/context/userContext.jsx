7f6139b1ded4dd06488f7eb49ecc4a913d334340
Joel Kram <joelnskram@gmail.com>
2025-06-25 16:05:01 +0100

    add: user provider context

---
+++ src/context/userContext.jsx
@@ -0,0 +1,12 @@
+import { createContext } from 'react';
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const loggedInUser = { name: 'test1', id: 6 };
+
+  return (
+    <UserContext.Provider value={{ loggedInUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
