31af08fba448ddfbf9fdf9c71c9735de6a72d157
Joel Kram <joelnskram@gmail.com>
2025-06-26 09:28:40 +0100

    impl: comment deletion from db but not ui

--- src/components/comments/DeleteButton.jsx
+++ src/components/comments/DeleteButton.jsx
@@ -1,5 +1,32 @@
 import { Button } from 'react-bootstrap';
-export const DeleteButton = ({comment_id}) => {
-  
-  return <Button variant="secondary">Delete</Button>;
+import { deleteComment } from '../../utils/deleteComment';
+import { useState } from 'react';
+export const DeleteButton = ({ comment_id }) => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDeletion = async () => {
+    if (isLoading) return;
+    setIsLoading(true);
+    try {
+      await deleteComment(comment_id);
+      setIsLoading(false);
+    } catch (err) {
+      setError(err);
+      setIsLoading(false);
+    }
+  };
+  // TODO Make this pretty later
+  if (error) {
+    return <p>Error in deleting comment</p>;
+  }
+  // TODO Make this pretty later
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <Button variant="secondary" onClick={handleDeletion}>
+      Delete
+    </Button>
+  );
 };
