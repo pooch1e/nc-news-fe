5b7a236fa3d9c008ddb1036c7effe23cde23c2f4
Joel Kram <joelnskram@gmail.com>
2025-06-26 08:23:41 +0100

    added: user context to app

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,29 @@ import { Layout } from './components/layout/Layout';
 import { Header } from './components/layout/Header';
 import { Article } from '../src/components/articles/Article';
 import { Footer } from './components/layout/Footer';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
 
 function App() {
+  const UserContext = createContext(null);
   const [postType, setPostType] = useState('article');
+  const [currentUser, setCurrentUser] = useState({ username: 'tickle122' });
   return (
     <>
       <RefreshProvider>
-        <idTypeContext.Provider value={{ postType, setPostType }}>
-          <header>
-            <Header />
-          </header>
-          <Routes>
-            <Route path="/" element={<Layout />} />
-            <Route path="/articles/:article_id" element={<Article />} />
-          </Routes>
-          <footer>
-            <Footer />
-          </footer>
-        </idTypeContext.Provider>
+        <UserContext.Provider value={{currentUser, setCurrentUser}}>
+          <idTypeContext.Provider value={{ postType, setPostType }}>
+            <header>
+              <Header />
+            </header>
+            <Routes>
+              <Route path="/" element={<Layout />} />
+              <Route path="/articles/:article_id" element={<Article />} />
+            </Routes>
+            <footer>
+              <Footer />
+            </footer>
+          </idTypeContext.Provider>
+        </UserContext.Provider>
       </RefreshProvider>
     </>
   );
--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,2 +1,11 @@
 import { createContext } from 'react';
 export const UserContext = createContext();
+export const UserProvider = ({ children }) => {
+  const loggedInUser = { name: 'test1', id: 6 };
+
+  return (
+    <UserContext.Provider value={{ loggedInUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
